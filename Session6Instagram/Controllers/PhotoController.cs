030437e6ee65f1cdee5d078fbfb671808ad4b68a
tscarlato <tscarlato@gmail.com>
2018-07-21 11:52:54 -0400

    this is the noon update

--- Session6Instagram/Controllers/PhotoController.cs
+++ Session6Instagram/Controllers/PhotoController.cs
@@ -1,6 +1,9 @@
 ﻿using Session6Instagram.Models;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -40,6 +43,45 @@ namespace Session6Instagram.Controllers
         {
             var database = new InstagramDbContext();
             return View(database.Photos.OrderByDescending(p => p.Date).ToList());
+        }
+
+        public ActionResult GetImage(int id)
+        {
+            var database = new InstagramDbContext();
+            var photo = database.Photos.Find(id);
+            using (var ms = new MemoryStream(photo.Picture))
+            {
+                var image = System.Drawing.Image.FromStream(ms);
+
+                var ratioX = (double)300 / image.Width;
+                var ratioY = (double)300 / image.Height;
+                var ratio = Math.Min(ratioX, ratioY);
+
+                var width = (int)(image.Width * ratio);
+                var height = (int)(image.Height * ratio);
+
+                var newImage = new System.Drawing.Bitmap(width, height);
+                System.Drawing.Graphics.FromImage(newImage).DrawImage(image, 0, 0, width, height);
+                System.Drawing.Bitmap bmp = new System.Drawing.Bitmap(newImage);
+
+                System.Drawing.ImageConverter converter = new System.Drawing.ImageConverter();
+
+                photo.Picture = (byte[])converter.ConvertTo(bmp, typeof(byte[]));
+
+                var stream = new MemoryStream(photo.Picture);
+
+                var bitmap = new Bitmap(stream);
+
+                var newstream = new MemoryStream();
+                bitmap.Save(newstream, ImageFormat.Jpeg);
+                newstream.Position = 0;
+
+                return new FileStreamResult(newstream, "image/jpg");
+
+
+                //  return "data:image/*;base64," + Convert.ToBase64String(photo.Picture);
+
+            }
         }
     }
 }
